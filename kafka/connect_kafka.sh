b068602b95ce6bf8e2d4196831d68c567b7e548c
Brijgopal Bharadwaj <brijgopalb@iisc.ac.in>
2024-11-15 14:53:57 +0530

    updated configs

--- kafka/connect_kafka.sh
+++ kafka/connect_kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Connect Kafka brokers by updating their advertised listeners
-source *.env
+source .env
 
 # Add other brokers' URLs here
 OTHER_BROKER_LISTENERS="OUTSIDE://<broker_1_ngrok_url>,OUTSIDE://<broker_2_ngrok_url>,OUTSIDE://<broker_3_ngrok_url>"
