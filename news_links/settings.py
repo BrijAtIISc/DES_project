95a4cd39de8c5206d62cd0c594d79f97df956604
Brijgopal Bharadwaj <brijgopalb@iisc.ac.in>
2024-11-27 16:42:01 +0530

    added more links data

---
+++ news_links/settings.py
@@ -0,0 +1,10 @@
+GCSJ_API_KEYS = []
+GCSJ_ENGINE_IDS = []
+
+# ----------------------------------------------------------------------
+
+# Local overrides
+try:
+    from local_settings import *  # noqa
+except ImportError:
+    pass
